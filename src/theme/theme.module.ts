a427abd504ee1f9b1014f20db47bb296959467d0
Victor Antonino Costa <victor.antonino12@gmail.com>
2022-10-25 00:05:52 -0300

    update: add services and controllers in theme module

--- src/theme/theme.module.ts
+++ src/theme/theme.module.ts
@@ -1,12 +1,14 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
+import { themeController } from "./controllers/theme.controller";
 import { Theme } from "./entities/theme.entity";
+import { ThemeService } from "./services/theme.service";
 
 @Module ({
     imports: [TypeOrmModule.forFeature([Theme])],
-    providers: [],
-    controllers: [],
+    providers: [ThemeService],
+    controllers: [themeController],
     exports: [TypeOrmModule]
 })
 export class ThemeModule {}
