df2e4c03a4628108428a516c1b38d7e730edcf3f
Victor Antonino Costa <victor.antonino12@gmail.com>
2022-10-25 01:23:47 -0300

    feat: create user module

---
+++ src/users/user.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])], 
+  providers: [],
+  controllers: [],
+  exports: [],
+})
+export class UserModule {}
