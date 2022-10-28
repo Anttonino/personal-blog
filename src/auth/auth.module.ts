dae421600b40e1067f2d138e72ebe212702f38a4
Anttonino <victor.antonino12@gmail.com>
2022-10-28 16:34:41 -0300

    feat: update auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-
-import { Bcrypt } from './bcrypt/bcrypt';
+import { JwtModule } from '@nestjs/jwt/dist';
+import { PassportModule } from '@nestjs/passport';
+import { UserModule } from '../users/user.module';
+import { jwtConstants } from './constants/constants';
+import { Bcrypt } from './entities/bcrypt/bcrypt';
+import { AuthService } from './services/auth.service';
+import { LocalStrategy } from './strategy/local.strategy';
 
 @Module({
-  imports: [],
-  providers: [Bcrypt],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register ({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
+  providers: [Bcrypt, AuthService, LocalStrategy],
   controllers: [],
   exports: [Bcrypt],
 })
