f7b66df5c8b5aeca372cbd37bc126098fa6a4ebe
Anttonino <victor.antonino12@gmail.com>
2022-10-31 16:15:04 -0300

    feat: add api property for each entity

--- src/theme/entities/theme.entity.ts
+++ src/theme/entities/theme.entity.ts
@@ -1,17 +1,22 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty } from "class-validator"
-import { Posting } from "src/posting/entities/posting.entity"
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm"
+
+import { Posting } from "../../posting/entities/posting.entity"
 
 @Entity ({name: "theme_tb"})
 export class Theme {
 
-    @PrimaryGeneratedColumn ()    
+    @PrimaryGeneratedColumn ()
+    @ApiProperty ()    
     id: number
 
     @IsNotEmpty ()
     @Column ({length: 300, nullable: false})
+    @ApiProperty ()
     description: string
 
+    @ApiProperty() 
     @OneToMany (() => Posting, (posting) => posting.theme)
     posting: Posting []
     
