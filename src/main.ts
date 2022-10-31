e4f097af3bcb17a742e01205d5d2ad71c1c8ad8a
Anttonino <victor.antonino12@gmail.com>
2022-10-31 15:57:25 -0300

    feat: add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 
 import { AppModule } from './app.module';
 
 async function bootstrap () {
   const app = await NestFactory.create (AppModule);
+
+  const config = new DocumentBuilder ()
+  .setTitle ('Individual Blog')
+  .setDescription ('Individual Blog Project')
+  .setContact ("Victor Antonino", "https://github.com/Anttonino", "victor.antonino12@gmail.com")
+  .setVersion ('1.0')
+  .addBearerAuth ()
+  .build ()
+  const document = SwaggerModule.createDocument (app, config)
+  SwaggerModule.setup ('/swagger', app, document);
+
   process.env.TZ = '-03:00';
 
   app.useGlobalPipes (new ValidationPipe ());
