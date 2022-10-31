9deda16edec4b5ccc4e20c004bf3ed5de0aa6574
Anttonino <victor.antonino12@gmail.com>
2022-10-31 15:23:08 -0300

    fix: import auth service in local strategy

--- src/auth/strategy/local.strategy.ts
+++ src/auth/strategy/local.strategy.ts
@@ -2,6 +2,8 @@ import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy } from "passport-jwt";
 
+import { AuthService } from "../services/auth.service";
+
 @Injectable ()
 
 export class LocalStrategy extends PassportStrategy (Strategy) {
