aa5edb6123cf7e5ffc4051b78e7cd47dad56e278
Anttonino <victor.antonino12@gmail.com>
2022-10-31 16:20:35 -0300

    feat: add api tag in auth controller

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,8 +1,11 @@
 import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
+
 import { UserLogin } from "../entities/userlogin.entity";
 import { LocalAuthGuard } from "../guard/local-auth.guard";
 import { AuthService } from "../services/auth.service";
 
+@ApiTags ('Usuario')
 @Controller ("/auth")
 export class AuthController {
     constructor (private authService: AuthService) { }
